3a54cd8a9cbb3b352453821b19195115e79e019a
Andrii Pankovetskyi <pankovskiy@kasseler-cms.net>
2017-11-03 18:54:25 +0200

    add event onClick for button

--- calculator/index.js
+++ calculator/index.js
@@ -1,1 +1,31 @@
-// jQuery('.button').
+(function () {
+    var input = jQuery('.btn');
+    var textarea = jQuery('input[type="text"]');
+    input.on('click', function () {
+        textarea.prop('value', textarea.prop('value') + jQuery(this).prop('value'));
+    });
+
+    var buttonEquals = jQuery('.btn-equals');
+    buttonEquals.on('click', function () {
+       var str = textarea.prop('value');
+       var arrayString = str.split(/(\*|\+)/);
+       var result = 0;
+       for (var i = 0; i < arrayString.length; i++) {
+        switch (arrayString[i]) {
+            case '+':
+                result+=Number(arrayString[i-1]) + Number(arrayString[i+1]);
+                break;
+            case '-':
+                result+=Number(arrayString[i-1]) - Number(arrayString[i+1]);
+                break;
+            case '*':
+                result+=Number(arrayString[i-1]) * Number(arrayString[i+1]);
+                break;
+            case '/':
+                result+=Number(arrayString[i-1]) / Number(arrayString[i+1]);
+                break;
+           }
+       }
+       textarea.prop('value', result);
+    });
+})();
