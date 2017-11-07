5f569e1b0a7cc702d2d837d6699c0784a01da17a
Andrii Pankovetskyi <pankovskiy@kasseler-cms.net>
2017-11-07 17:20:54 +0200

    operation '-', '+', '*', '/'

--- calculator/index.js
+++ calculator/index.js
@@ -4,14 +4,16 @@
     input.on('click', function () {
         textarea.prop('value', textarea.prop('value') + jQuery(this).prop('value'));
     });
-    var arrayMemory = [];
     var buttonEquals = jQuery('.btn-equals');
     buttonEquals.on('click', function test() {
        var str = textarea.prop('value');
-       var arrayString = str.split(/(\*|\+)/);
+       var arrayString = str.split(/(\*|\+|\-|\/)/);
        var result = 0;
        var count = 0;
        while (count < arrayString.length) {
+           if (arrayString.indexOf('*') != -1) {
+               count = arrayString.indexOf('*');
+           }
            switch (arrayString[count]) {
                case '+':
                    result=Number(arrayString[count-1]) + Number(arrayString[count+1]);
@@ -19,13 +21,20 @@
                    count=0;
                    break;
                case '-':
-                   result+=Number(arrayString[i-1]) - Number(arrayString[i+1]);
+                   result=Number(arrayString[count-1]) - Number(arrayString[count+1]);
+                   arrayString.splice(count-1, 3, result);
+                   count=0;
+
                    break;
                case '*':
-                   result+=Number(arrayString[i-1]) * Number(arrayString[i+1]);
+                   result=Number(arrayString[count-1]) * Number(arrayString[count+1]);
+                   arrayString.splice(count-1, 3, result);
+                   count=0;
                    break;
                case '/':
-                   result+=Number(arrayString[i-1]) / Number(arrayString[i+1]);
+                   result=Number(arrayString[count-1]) / Number(arrayString[count+1]);
+                   arrayString.splice(count-1, 3, );
+                   count=0;
                    break;
            }
        count++;
@@ -38,9 +47,9 @@
     function addResultToMemory (value) {
         if (jQuery('#selectMemory').children().length >= 10) {
             jQuery('#selectMemory').children().last().remove();
-            jQuery('#selectMemory').prepend(`<option value=${value}>${value}</option>`);
+            jQuery('#selectMemory').prepend(`<option value=${value} selected>${value}</option>`);
         } else {
-            jQuery('#selectMemory').prepend(`<option value=${value}>${value}</option>`);
+            jQuery('#selectMemory').prepend(`<option value=${value} selected>${value}</option>`);
         }
     }
     var selectMemory = jQuery('#selectMemory');
